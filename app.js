5cb138c6a2a7660524797d891e27c38d1965a7f1
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-11 01:55:16 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// var s = document.getElementsByClassName("images")
+// for (var i = 0 ; i < s.length;i++){
+//     s[i].src="https://t4.ftcdn.net/jpg/04/60/49/11/360_F_460491148_TG2fTN9oV5NEgpNbN8l8TDlnN8NIHRpt.jpg"
+//     console.log('vaalue of img.  '+ i)
+//     console.dir(s[i].src)
+// }
+var b =document.getElementsByClassName("hhhh")
+console.log(b)
